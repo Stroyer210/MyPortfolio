013f8ce148ea68b95d8447363e373d4c991de2c1
Stroyer210 <sergiosthiwar@gmail.com>
2024-04-22 21:32:40 -0500

    adding a favicon and changed the title

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 // import Skills from "./components/Skills";
 // import Work from "./components/Work";
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    document.title = "Sergio's portfolio"
+  }, [])
   return (
     <div>
+      <head>
+        <title>Sergio</title>
+      </head>
       <Navbar/>
       <Outlet />
       <Footer />
